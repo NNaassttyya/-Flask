03eeda44e6d097be6f6b61b0ed131c8e4f79b0c6
NNaassttyya <152288121+NNaassttyya@users.noreply.github.com>
2025-06-11 13:15:39 +0500

    Update main.js
    добавила пробел

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,7 @@ const swiper = new Swiper(".swiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
